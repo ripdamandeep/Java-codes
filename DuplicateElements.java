d212c9cc46715748c165d98e0525f07c7af2c116
ripdamandeep <68321983+ripdamandeep@users.noreply.github.com>
2020-07-23 11:30:33 +0530

    Add files via upload

---
+++ DuplicateElements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class DuplicateElement
+{
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt(),array[] = new int[n];
+	        for (int i = 0; i < n; i++) array[i] = sc.nextInt();
+
+	        ArrayList<Integer> dup=new ArrayList<>();
+	        for(int i=0;i<n;i++) array[array[i]%n]+=n;
+	        for(int i=0;i<n;i++) if(array[i]/n>1) dup.add(i);
+	        if(dup.isEmpty()) dup.add(-1);
+	        for (Integer val : dup) System.out.print(val + " ");
+	        System.out.println();
+	    }
+	}
+
